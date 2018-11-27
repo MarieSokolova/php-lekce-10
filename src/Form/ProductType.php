b903e85899bba3162f843d6853a197a49c916e00
Marie <>
2018-11-27 08:43:42 +0000

    lekce-10

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Product;
+use App\Entity\Manufacturer;
+use App\Entity\Supplier;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProductType extends AbstractType
 {
@@ -15,7 +18,17 @@ class ProductType extends AbstractType
             ->add('name')
             ->add('price')
             ->add('description')
-        ;
+            ->add('manufacturer', EntityType::class, [
+    'class' => Manufacturer::class, 
+    'choice_label' => 'name'
+  ])
+            ->add('supplier', EntityType::class, [
+  'class' => Supplier::class, 
+  'choice_label' => 'name', 
+  'multiple' => true, 
+  'expanded' => true
+])
+  ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
